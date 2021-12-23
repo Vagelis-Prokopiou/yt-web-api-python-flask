80c9f47b7f0a88e84373af0e80577e36e3b1377b
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-23 11:02:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# https://auth0.com/blog/developing-restful-apis-with-python-and-flask/
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+def getUsers():
+    users = []
+    for index in range(1, 1001):
+        strIndex = str(index)
+        firstName = "FirstName" + strIndex
+        lastName = "LastName" + strIndex
+        age = 25
+        framework = "Python (Flask)"
+        users.append({
+            "index": index,
+            "FirstName": firstName,
+            "LastName": lastName,
+            "age": 25,
+            "framework": framework,
+        })
+    return users
+
+
+@app.route("/users")
+def users():
+    return jsonify(getUsers())
